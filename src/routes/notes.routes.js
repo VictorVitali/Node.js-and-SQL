57a19dd0ebaa641820b7f06d9b4816bafc34f332
Victor Vitali <victorvitali@gmail.com>
2024-06-11 14:45:22 -0300

    add create tags and link

---
+++ src/routes/notes.routes.js
@@ -0,0 +1,18 @@
+const { Router } = require("express");
+const notesRoutes = Router();
+
+const NotesController = require("../controllers/NotesControllers");
+const notesController = new NotesController();
+
+//function myMiddleware(req, res, next) {
+//    const {isAdmin} = req.body;
+//
+//   if(!req.body.isAdmin) {
+//        return res.json({ message: "user isnt a admin" });
+//    };
+//    next();
+//}
+
+notesRoutes.post("/:user_id", notesController.create);
+
+module.exports = notesRoutes;
